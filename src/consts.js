ceb4a5562d9a0dc36f3579f133d2fc6fe43e55a3
Jeremy <ucabsjc@ucl.ac.uk>
2020-08-28 14:18:57 +0100

    changed domain from localhost to ip address

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 /** application domain */
-export const DOMAIN = 'http://localhost:3000'
+export const DOMAIN = 'http://139.162.245.237:3000'
 
 /** backend address */
 export const BACKEND_ADDRESS = 'http://178.79.148.73:8080'
