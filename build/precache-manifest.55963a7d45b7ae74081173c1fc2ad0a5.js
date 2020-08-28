0d7fac7eadb4d26e531546e1f8f9fbe2ba5ca72c
Jeremy <30488840+scjc@users.noreply.github.com>
2020-08-28 11:38:50 +0100

    Move project to root (#9)
    * moved web app project to root of repo
    
    * added deployment script
    
    * updated print output during deployment

---
+++ build/precache-manifest.55963a7d45b7ae74081173c1fc2ad0a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec372e39654222aa18eb1921fed79214",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee3099c0321d1e71bc74",
+    "url": "/static/css/2.6a1a3c27.chunk.css"
+  },
+  {
+    "revision": "c29396ab780f632f3bf8",
+    "url": "/static/css/main.09c40082.chunk.css"
+  },
+  {
+    "revision": "ee3099c0321d1e71bc74",
+    "url": "/static/js/2.596d5310.chunk.js"
+  },
+  {
+    "revision": "ad779b788669fe4c3f59e1bf342ca03c",
+    "url": "/static/js/2.596d5310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29396ab780f632f3bf8",
+    "url": "/static/js/main.7bd80dc6.chunk.js"
+  },
+  {
+    "revision": "2877002edf0247349384",
+    "url": "/static/js/runtime-main.e7fba56a.js"
+  }
+]);
