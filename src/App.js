363ce1fe52c9c957baac2e761c00141c6bf6a518
Jeremy <scjc885@gmail.com>
2020-08-29 06:15:12 +0100

    fixed broweser router

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import OauthCallBackPage from "./views/OauthCallBackPage"
 import {WireframesPage} from "./views/WireframesPage"
 import {CodeConvertPage} from "./views/CodeConvertPage"
 
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
     return (
-        <div className="App">
-            <Switch>
-                <Route exact path='/' component={AuthenticateHomePage}/>
-                <Route exact path='/auth' component={OauthCallBackPage}/>
-                <Route exact path='/wireframes' component={WireframesPage}/>
-                <Route exact path='/convert' component={CodeConvertPage}/>
-            </Switch>
-        </div>
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route exact path='/' component={AuthenticateHomePage}/>
+                    <Route exact path='/auth' component={OauthCallBackPage}/>
+                    <Route exact path='/wireframes' component={WireframesPage}/>
+                    <Route exact path='/convert' component={CodeConvertPage}/>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
