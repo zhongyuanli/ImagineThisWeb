54c49fe09d69d8afbb894e6c0600c69a21515882
Vojta Adam <bluetrox@hotmail.com>
2020-08-31 15:57:57 +0200

    Uploading local setup

--- imaginethisweb/src/consts.js
+++ imaginethisweb/src/consts.js
@@ -2,4 +2,4 @@
 export const DOMAIN = 'http://localhost:3000'
 
 /** backend address */
-export const BACKEND_ADDRESS = 'http://178.79.148.73:8080'
+export const BACKEND_ADDRESS = 'http://localhost:8080'
