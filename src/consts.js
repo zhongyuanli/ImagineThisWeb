f4415af0b1956ede1fc256d604a60e3ad86c6bbe
Jeremy <scjc885@gmail.com>
2020-09-24 22:27:33 +0100

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,8 +1,7 @@
 /** application domain */
 // export const DOMAIN = 'http://139.162.245.237'
 export const DOMAIN = 'http://localhost:3000'
-// export const DOMAIN = 'http://212.71.245.46'
-export const DOMAIN = 'http://localhost:3000'
+
 
 
 /** backend address */
