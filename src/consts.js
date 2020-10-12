2616df4ef90598356de909610952c1f214591f9b
Jeremy <ucabsjc@ucl.ac.uk>
2020-10-12 04:58:13 +0100

    updated client id to be in const.js for easy access

--- src/consts.js
+++ src/consts.js
@@ -5,6 +5,13 @@ export const DOMAIN = 'http://88.80.186.99'
 /** backend address */
 export const BACKEND_ADDRESS = 'http://139.162.239.246:8080'
 
+/** Figma Client ID */
+export const CLIENT_ID = '30GYpNuEA2obzNnPS51yKg'
+
+// for localhost, use this client_id
+// export const CLIENT_ID = 'C0nipA1SCO3Nmym63jVLth'
+
+
 /** Figma API client secret key */
 export const CLIENT_SECRET = 'RwZdDhR5IJxwwsFUtv8eE1ARFFTmm5'
 
