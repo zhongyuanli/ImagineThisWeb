5bca6f98d76c93c434aa31065cf8a7bf3f62d726
Jeremy <ucabsjc@ucl.ac.uk>
2020-10-12 08:51:48 +0100

    reordered const.js

--- src/consts.js
+++ src/consts.js
@@ -1,20 +1,12 @@
-/** application domain */
-export const DOMAIN = 'http://88.80.186.99'
-// export const DOMAIN = 'http://localhost:3000'
-
-/** backend address */
-export const BACKEND_ADDRESS = 'http://139.162.239.246:8080'
-
-/** Figma Client ID */
-export const CLIENT_ID = '30GYpNuEA2obzNnPS51yKg'
-
-// for localhost, use this client_id
-// export const CLIENT_ID = 'C0nipA1SCO3Nmym63jVLth'
+/** deployed version */
+// export const DOMAIN = 'http://88.80.186.99'
+// export const CLIENT_ID = '30GYpNuEA2obzNnPS51yKg'
+// export const CLIENT_SECRET = 'RwZdDhR5IJxwwsFUtv8eE1ARFFTmm5' 
+// export const BACKEND_ADDRESS = 'http://139.162.239.246:8080'
 
 
-/** Figma API client secret key */
-export const CLIENT_SECRET = 'RwZdDhR5IJxwwsFUtv8eE1ARFFTmm5'
-
-// for localhost, use this api key
-// export const CLIENT_SECRET = 'ARJJi2cIa7pQLUhN6bJ2d93fWFALfe'
-
+/** LOCALHOST, use the following */ 
+export const CLIENT_ID = 'C0nipA1SCO3Nmym63jVLth'
+export const DOMAIN = 'http://localhost:3000'
+export const CLIENT_SECRET = 'ARJJi2cIa7pQLUhN6bJ2d93fWFALfe' // Figma API secret key
+export const BACKEND_ADDRESS = 'http://localhost:8080'
