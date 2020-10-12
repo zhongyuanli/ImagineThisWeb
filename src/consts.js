daf82c05e9704850362ba1d4ef44ce3df3cca563
Jeremy <ucabsjc@ucl.ac.uk>
2020-10-12 04:48:08 +0100

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -6,4 +6,8 @@ export const DOMAIN = 'http://88.80.186.99'
 export const BACKEND_ADDRESS = 'http://139.162.239.246:8080'
 
 /** Figma API client secret key */
-export const CLIENT_SECRET = 'ARJJi2cIa7pQLUhN6bJ2d93fWFALfe'
+export const CLIENT_SECRET = 'RwZdDhR5IJxwwsFUtv8eE1ARFFTmm5'
+
+// for localhost, use this api key
+// export const CLIENT_SECRET = 'ARJJi2cIa7pQLUhN6bJ2d93fWFALfe'
+
