4bf3990a7802285868bf39ab9603c6c27f203b23
Jeremy <ucabsjc@ucl.ac.uk>
2020-10-12 05:01:47 +0100

    updated redirect url to be dynamic

--- src/views/AuthenticateHomePage.js
+++ src/views/AuthenticateHomePage.js
@@ -115,7 +115,7 @@ export class AuthenticateHomePage extends Component {
     * (Redirect to Figma's OAuth API)
     */
     oauthRedirect() {
-        window.location.href = "https://www.figma.com/oauth?client_id=HbTuw2lrfAC84htJy0Rtf1&redirect_uri="+DOMAIN+"/auth&scope=file_read&state=get_token&response_type=code"
+        window.location.href = "https://www.figma.com/oauth?client_id="+CLIENT_ID+"&redirect_uri="+DOMAIN+"/auth&scope=file_read&state=get_token&response_type=code"
     }
 
     render() {
