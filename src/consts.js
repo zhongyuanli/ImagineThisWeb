e555c23a0de21481b73ac02e98f93b7a74d7d974
Jeremy <ucabsjc@ucl.ac.uk>
2020-10-13 04:55:02 +0100

    added sumbission consts

--- src/consts.js
+++ src/consts.js
@@ -4,6 +4,12 @@ export const CLIENT_ID = 'ZSjQ3iai2GyJYhAXq1fI2D'
 export const CLIENT_SECRET = '64wrt6gPjuFdsnZCtpKj29ZQcjpvVN' 
 export const BACKEND_ADDRESS = 'http://139.162.239.246:8080'
 
+/** submission version */
+// export const DOMAIN = 'http://212.71.245.46'
+// export const CLIENT_ID = '2gRf1C2m9LTJBLURdJb24U'
+// export const CLIENT_SECRET = 'rDFRNCmADh2R5qzOwTfjMCM7HuXx7C' 
+// export const BACKEND_ADDRESS = 'http://139.162.215.16:8080'
+
 
 /** LOCALHOST, use the following */ 
 // export const CLIENT_ID = 'C0nipA1SCO3Nmym63jVLth'
