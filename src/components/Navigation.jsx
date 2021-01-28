f8c70aa69caa38d17f04fba88e52e41afddae047
Karish Singh <karish.singh28@gmail.com>
2021-01-28 02:46:18 +0000

    Modified search bar according to wanted changes. Position moved to right of the navbar and round edge along with other minor changes.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -53,7 +53,7 @@ class Navigation extends Component {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
+                        <Nav className="">
                             <Nav.Link href="https://imaginethisucl.github.io/getting%20started/how%20to%20use.html">Get Started</Nav.Link>
                             <Nav.Link href="https://imaginethisucl.github.io/guidelines/design%20introduction.html">Guidelines</Nav.Link>
 
@@ -86,34 +86,10 @@ class Navigation extends Component {
                         {/*    /!*</div>*!/*/}
                         {/*</Form>*/}
 
-                       <div className="input-group">
-                           <Form onSubmit={this.handleSubmit}>
-                           <InputGroup className="input-group-prepend">
-                               <InputGroup.Prepend>
-                                   <Button variant="btn btn-light search-button" type="submit">
-                                       <svg xmlns="http://www.w3.org/2000/svg"
-                                            width="16" height="16"
-                                            fill="currentColor"
-                                            className="bi bi-search"
-                                            viewBox="0 0 16 16">
-                                           <path
-                                               d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                                           </svg>
-                                   </Button>
-                               </InputGroup.Prepend>
-                               <FormControl
-                                   className="form-control navbar-input"
-                                   aria-describedby="basic-addon1"
-                                   placeholder="Find Project With ID"
-                                   value={this.state.value}
-                                   onChange={this.handleChange}
-                               />
-                           </InputGroup>
-                           </Form>
-                       </div>
+                       {/*<div className="input-group">*/}
 
                         </Nav>
-                        <Nav>
+                        <Nav className="mr-auto">
                             <NavDropdown
                               alignRight
                               title="GitHub"
@@ -124,6 +100,34 @@ class Navigation extends Component {
                                 <NavDropdown.Item href="https://github.com/ImagineThisUCL/ImagineThis-Mobile">Mobile Components</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
+
+                        <Nav>
+                            <Form onSubmit={this.handleSubmit} className="input-group">
+                                <InputGroup className="input-group-prepend">
+                                    <FormControl
+                                        className="form-control navbar-input"
+                                        aria-describedby="basic-addon1"
+                                        placeholder="Find Project With ID"
+                                        value={this.state.value}
+                                        onChange={this.handleChange}
+                                    />
+                                    <InputGroup.Append>
+                                        <Button variant="btn btn-light search-button" type="submit">
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                 width="16" height="16"
+                                                 fill="currentColor"
+                                                 className="bi bi-search"
+                                                 viewBox="0 0 16 16">
+                                                <path
+                                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                            </svg>
+                                        </Button>
+                                    </InputGroup.Append>
+                                </InputGroup>
+                            </Form>
+                        </Nav>
+
+
                     </Navbar.Collapse>
                 </Navbar>
             </div>
