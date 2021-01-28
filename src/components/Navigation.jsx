e8ddf89b64663690c82621b065b9fd88ea564f67
Zhongyuan Li <zczlz91@ucl.ac.uk>
2021-01-28 16:41:56 +0800

    empty check

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -17,21 +17,24 @@ import Button from "react-bootstrap/Button"
 class Navigation extends Component {
     constructor(props) {
         super(props);
-        this.state = {value:''}
+        this.state = { value: '' }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event){
-        this.setState({value:event.target.value});
+    handleChange(event) {
+        this.setState({ value: event.target.value });
     }
 
-    handleSubmit(event){
+    handleSubmit(event) {
         if (this.state.value === '223') {
             alert('ID:' + this.state.value + 'Found');
         }
-        else{
+        else if (this.state.value == "") {
+            alert("ID can not be empty");
+        }
+        else {
             alert('ID Not Found')
         }
 
@@ -39,16 +42,16 @@ class Navigation extends Component {
     }
 
     render() {
-        return(
+        return (
             <div className="guide-bar">
                 <Navbar collapseOnSelect expand="lg" className="navbar-style" variant="dark">
                     <Navbar.Brand href="/" className="navbar-brand">
                         <img
-                          alt="Imagine This logo"
-                          src={Logo}
-                          className="d-inline-block align-top"
-                          width="165"
-                          height="30"
+                            alt="Imagine This logo"
+                            src={Logo}
+                            className="d-inline-block align-top"
+                            width="165"
+                            height="30"
                         />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -57,43 +60,43 @@ class Navigation extends Component {
                             <Nav.Link href="https://imaginethisucl.github.io/getting%20started/how%20to%20use.html">Get Started</Nav.Link>
                             <Nav.Link href="https://imaginethisucl.github.io/guidelines/design%20introduction.html">Guidelines</Nav.Link>
 
-                        {/*   <Form className="input-group" onSubmit={this.handleSubmit}>*/}
-                        {/*    /!*<div className="input-group">*!/*/}
-                        {/*        <div className="input-group-prepend">*/}
-                        {/*            <button*/}
-                        {/*                className="btn btn-light search-button"*/}
-                        {/*                onClick=""*/}
-                        {/*                type="button">*/}
-                        {/*                <svg xmlns="http://www.w3.org/2000/svg"*/}
-                        {/*                     width="16" height="16"*/}
-                        {/*                     fill="currentColor"*/}
-                        {/*                     className="bi bi-search"*/}
-                        {/*                     viewBox="0 0 16 16">*/}
-                        {/*                    <path*/}
-                        {/*                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>*/}
-                        {/*                </svg>*/}
-                        {/*            </button>*/}
-                        {/*        </div>*/}
+                            {/*   <Form className="input-group" onSubmit={this.handleSubmit}>*/}
+                            {/*    /!*<div className="input-group">*!/*/}
+                            {/*        <div className="input-group-prepend">*/}
+                            {/*            <button*/}
+                            {/*                className="btn btn-light search-button"*/}
+                            {/*                onClick=""*/}
+                            {/*                type="button">*/}
+                            {/*                <svg xmlns="http://www.w3.org/2000/svg"*/}
+                            {/*                     width="16" height="16"*/}
+                            {/*                     fill="currentColor"*/}
+                            {/*                     className="bi bi-search"*/}
+                            {/*                     viewBox="0 0 16 16">*/}
+                            {/*                    <path*/}
+                            {/*                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>*/}
+                            {/*                </svg>*/}
+                            {/*            </button>*/}
+                            {/*        </div>*/}
 
-                        {/*        <input*/}
-                        {/*            className={"form-control navbar-input"}*/}
-                        {/*            // placeholder="Find Project With ID"*/}
-                        {/*            name={this.projectID}*/}
-                        {/*            value={this.state.value}*/}
-                        {/*            onChange={this.handleChange}*/}
+                            {/*        <input*/}
+                            {/*            className={"form-control navbar-input"}*/}
+                            {/*            // placeholder="Find Project With ID"*/}
+                            {/*            name={this.projectID}*/}
+                            {/*            value={this.state.value}*/}
+                            {/*            onChange={this.handleChange}*/}
 
-                        {/*        />*/}
-                        {/*    /!*</div>*!/*/}
-                        {/*</Form>*/}
+                            {/*        />*/}
+                            {/*    /!*</div>*!/*/}
+                            {/*</Form>*/}
 
-                       {/*<div className="input-group">*/}
+                            {/*<div className="input-group">*/}
 
                         </Nav>
                         <Nav className="mr-auto">
                             <NavDropdown
-                              alignRight
-                              title="GitHub"
-                              id="collapsible-nav-dropdown"
+                                alignRight
+                                title="GitHub"
+                                id="collapsible-nav-dropdown"
                             >
                                 <NavDropdown.Item href="https://github.com/ImagineThisUCL/ImagineThisWeb">Web App</NavDropdown.Item>
                                 <NavDropdown.Item href="https://github.com/ImagineThisUCL/ImagineThisServer">Server</NavDropdown.Item>
@@ -114,12 +117,12 @@ class Navigation extends Component {
                                     <InputGroup.Append>
                                         <Button variant="btn btn-light search-button" type="submit">
                                             <svg xmlns="http://www.w3.org/2000/svg"
-                                                 width="16" height="16"
-                                                 fill="currentColor"
-                                                 className="bi bi-search"
-                                                 viewBox="0 0 16 16">
+                                                width="16" height="16"
+                                                fill="currentColor"
+                                                className="bi bi-search"
+                                                viewBox="0 0 16 16">
                                                 <path
-                                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                                             </svg>
                                         </Button>
                                     </InputGroup.Append>
