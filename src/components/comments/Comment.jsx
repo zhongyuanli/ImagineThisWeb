28329f9b965b76f8e9d91bfb05bf0dbcdf500788
Zhongyuan Li <zczlz91@ucl.ac.uk>
2021-01-29 11:40:38 +0800

    add_voting_view
    the current voting is based on mock data

--- src/components/comments/Comment.jsx
+++ src/components/comments/Comment.jsx
@@ -1,18 +1,53 @@
 import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
+import '../../assets/upvote-icon-11.jpg'
+import '../../css/comments.css'
+
+// const sectionStyle = {
+//   width: '30px',
+//   height: '30px',
+//   backgroundImage: "url(../../ assets / upvote-icon-11.jpg')",
+// }
 
 class Comment extends Component {
   constructor(props) {
+
     super(props);
+    this.state = { upCount: 0, downCount: 0 };
   }
 
+  upvote = () => {
+    var upcount = this.state.upCount;
+    this.setState({ upCount: upcount + 1 });
+    // var upSpan = document.getElementById("upvote_count");
+    // var count = parseInt(upSpan.innerHTML);
+    // upSpan.innerHTML = count + 1;
+
+  }
+
+  downvote = () => {
+    var downcount = this.state.downCount;
+    this.setState({ downCount: downcount + 1 });
+  }
+
+
   render() {
+    const { upCount, downCount } = this.state;
     return (
       <div>
         <br />
         <Card bg="light">
           <Card.Header>
             {this.props.author} - {this.props.created}
+            <div className="vote_area" id="downvoting_area">
+              <button className="vote_icon downvote_icon" onClick={this.downvote}></button>
+              <span className="vote_count" id="downvote_count">{downCount}</span>
+            </div>
+            <div className="vote_area" id="upvoting_area">
+              <button className="vote_icon upvote_icon" onClick={this.upvote}></button>
+              <span className="vote_count" id="upvote_count" >{upCount}</span>
+            </div>
+
           </Card.Header>
           <Card.Body>
             {/* <Card.Title>{this.props.children}</Card.Title> */}
