53d84ead9d30614b04d6cd680c7b8486f67b00e0
Dino <dinohossain1@gmail.com>
2021-01-31 19:04:55 +0000

    Added up vote and down vote buttons

--- src/components/comments/Comment.jsx
+++ src/components/comments/Comment.jsx
@@ -4,6 +4,11 @@ import Card from "react-bootstrap/Card";
 class Comment extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      upvote: false,
+      downvote: false,
+      voteCount: 0,
+    };
   }
 
   render() {
@@ -13,6 +18,41 @@ class Comment extends Component {
         <Card bg="light">
           <Card.Header>
             {this.props.author} - {this.props.created}
+            <span>
+              <i
+                className={`icon icon-downvote ${
+                  this.state.downvote ? "voted" : null
+                }`}
+                onClick={() =>
+                  this.setState(
+                    {
+                      downvote: !this.state.downvote,
+                      upvote: false,
+                      voteCount: this.state.voteCount - 1,
+                    },
+                    console.log(this.state)
+                  )
+                }
+              ></i>
+            </span>
+            <span>{this.state.voteCount}</span>
+            <span>
+              <i
+                className={`icon icon-upvote ${
+                  this.state.upvote ? "voted" : null
+                }`}
+                onClick={() =>
+                  this.setState(
+                    {
+                      upvote: !this.state.upvote,
+                      downvote: false,
+                      voteCount: this.state.voteCount + 1,
+                    },
+                    console.log(this.state)
+                  )
+                }
+              ></i>
+            </span>
           </Card.Header>
           <Card.Body>
             {/* <Card.Title>{this.props.children}</Card.Title> */}
