59ff5c1e9596f7504d88d4f29730bb70e6f3bd57
Karish Singh <karish.singh28@gmail.com>
2021-02-01 20:05:52 +0000

    Added in seperate svg file for search button and replaced if statement with catch in search button form.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,6 +2,7 @@ import React, {
     Component
 } from 'react'
 import Logo from "../assets/ImagineThisLogo.png"
+import Search from "../assets/Search.svg"
 import Navbar from "react-bootstrap/Navbar"
 import NavDropdown from "react-bootstrap/NavDropdown"
 import Nav from "react-bootstrap/Nav"
@@ -35,19 +36,13 @@ class Navigation extends Component {
   handleSubmit(event) {
     var url = host + "/api/v1/projects/" + this.state.value + "/feedback"
     axios.get(url).then(res => {
-        if (res.status === 200) {
-          console.log(res.data);
-          // alert('ID:' + this.state.value + ' Found');
-          window.location.href="/comments"
-
-        } else if (this.state.value === "") {
-          alert("ID can not be empty");
-        } else {
-          alert("Project ID doesn't exist")
-          console.log("Not Here.")
-        }
+      console.log(res.data);
+      window.location.href = "/comments"
+    }).catch(function (error) {
+        alert("Project ID doesn't exist");
+        console.log("Not Here.")
       }
-    );
+    )
     event.preventDefault();
   }
 
@@ -104,14 +99,7 @@ class Navigation extends Component {
                   />
                   <InputGroup.Append>
                     <Button variant="btn btn-light search-button" type="submit">
-                      <svg xmlns="http://www.w3.org/2000/svg"
-                           width="16" height="16"
-                           fill="currentColor"
-                           className="bi bi-search"
-                           viewBox="0 0 16 16">
-                        <path
-                          d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                      </svg>
+                      <img alt="search button" src={Search}/>
                     </Button>
                   </InputGroup.Append>
                 </InputGroup>
