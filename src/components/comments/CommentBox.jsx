2395a9ea0bd5eb10f1b87aeb9854629bce3259ab
Zhongyuan Li <zczlz91@ucl.ac.uk>
2021-02-11 20:32:06 +0800

    change to comment style to the javadoc(in CommentBox.jsx)

--- src/components/comments/CommentBox.jsx
+++ src/components/comments/CommentBox.jsx
@@ -77,7 +77,10 @@ class CommentBox extends React.Component {
   // }
 
 
-  //basd on the scheme user chosed to perform sort
+  /**
+   * basd on the scheme user chosed to perform sort
+   * @param e
+   */
   sortComments(e){
     const allComments = this.state.comments;
     if(e == "1"){
@@ -94,7 +97,12 @@ class CommentBox extends React.Component {
 
   }
 
-  //sor the comments by the time they committed
+  /**
+   * sort the comments by the time they committed
+   *
+   * @param field
+   * @returns {function(*, *)}
+   */
   sortByTime(field){
       return function(a,b) {
         let x = new Date(a[field])/1000;
@@ -103,15 +111,24 @@ class CommentBox extends React.Component {
       }
   }
 
-  //sort the comments by the voting count
+
+  /**
+   * sort the comments by the voting count
+   * @param field
+   * @returns {function(*, *)}
+   */
   sortByVotes(field){
     return function(a,b){
       return b[field] - a[field];
     }
   }
 
-  //sort the comments based on two attribute,
-  //including the time and voting count
+  /**
+   * sort the comments based on two attribute,including the time and voting count
+   * @param field1
+   * @param field2
+   * @returns {function(*, *): *}
+   */
   sortByTwoFields(field1,field2){
     return function (a,b){
       if(a[field1] == b[field1]){
