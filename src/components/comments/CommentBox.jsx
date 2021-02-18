bddee51bf2f530b73a7ebf79a29dbebab388ac1f
Zhongyuan Li <zczlz91@ucl.ac.uk>
2021-02-18 19:51:10 +0800

    default sorting

--- src/components/comments/CommentBox.jsx
+++ src/components/comments/CommentBox.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import Badge from "react-bootstrap/Badge";
 import axios from "axios";
 import moment from "moment";
-import CommentList from "./CommentList";
-import CommentForm from "./CommentForm";
-import { LOCAL_HOST } from "../../consts";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import DropdownMenu from "react-bootstrap/DropdownMenu";
 import Dropdown from "react-bootstrap/Dropdown";
 import { v4 as uuidv4 } from 'uuid';
+import { LOCAL_HOST } from "../../consts";
+import CommentForm from "./CommentForm";
+import CommentList from "./CommentList";
 
 class CommentBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { comments: [], votedComments: {}, userID: "", userName: "" };
-
+    this.state = {
+      comments: [], votedComments: {}, userID: "", userName: "",
+    };
+    const allComments = this.state.comments;
+    const sortedArray = allComments.sort(this.sortByTwoFields('votes', 'timestamp'));
+    this.setState({ comments: sortedArray });
     this.getComments = this.getComments.bind(this);
     this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
     this.createAnonymousUser = this.createAnonymousUser.bind(this);
@@ -22,18 +26,17 @@ class CommentBox extends React.Component {
 
     // check if there is a user credential stored in localStorage
     // if not, create a new user credential
-    let storedUserStr = localStorage.getItem('user');
+    const storedUserStr = localStorage.getItem('user');
     if (storedUserStr === null) {
       this.createAnonymousUser();
     } else {
       // get user credential from localStorage
-      let storedUser = JSON.parse(storedUserStr);
+      const storedUser = JSON.parse(storedUserStr);
       this.state.userID = storedUser.userID;
       this.state.userName = storedUser.userName;
       // check if there's any vote associated to the current user
-      this.getVotesForUser()
+      this.getVotesForUser();
     }
-    
   }
 
   componentDidMount() {
@@ -84,30 +87,30 @@ class CommentBox extends React.Component {
       .catch((err) => {
         console.error(err);
       });
-  };
+  }
 
   async getVotesForUser() {
     await axios
       .get(`${LOCAL_HOST}/api/v1/users/${this.state.userID}/votes`)
-      .then(res => {
+      .then((res) => {
         if (res.status == 200 && res.data.length > 0) {
-          res.data.map(vote => {
-            this.state.votedComments[vote.feedbackId] = { voteID: vote.voteId, voteValue: vote.voteValue }
-          })
+          res.data.map((vote) => {
+            this.state.votedComments[vote.feedbackId] = { voteID: vote.voteId, voteValue: vote.voteValue };
+          });
         }
       })
-      .catch(err => console.log)
+      .catch((err) => console.log);
   }
 
   async createAnonymousUser() {
     // create an anonymous user
     console.log('Generating new user credential');
-    let userName = 'Anonymous User';
-    let userID = uuidv4();
+    const userName = 'Anonymous User';
+    const userID = uuidv4();
     // send a request to the server
     await axios
-      .post(`${LOCAL_HOST}/api/v1/users`, {userId: userID, userName})
-      .then(res => {
+      .post(`${LOCAL_HOST}/api/v1/users`, { userId: userID, userName })
+      .then((res) => {
         if (res.data.success) {
           console.log('Setting up local Storage');
           // update localStorage
@@ -116,60 +119,57 @@ class CommentBox extends React.Component {
           this.state.userName = userName;
         }
       })
-      .catch(err => {
-        console.log(err)
-      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   /**
    * Sorts feedback by the option the user chose to perform.
    */
-  sortComments(e){
+  sortComments(e) {
     const allComments = this.state.comments;
-    if(e == "1"){
-      let sortedArray = allComments.sort(this.sortByTime('timestamp'));
-      this.setState({comments: sortedArray});
-    }else if(e == "2"){
-      let sortedArray = allComments.sort(this.sortByVotes('votes'));
-      this.setState({comments: sortedArray});
-    }else if(e == "3"){
-      let sortedArray = allComments.sort(this.sortByTwoFields('votes','timestamp'));
-      this.setState({comments: sortedArray});
+    if (e == "1") {
+      const sortedArray = allComments.sort(this.sortByTime('timestamp'));
+      this.setState({ comments: sortedArray });
+    } else if (e == "2") {
+      const sortedArray = allComments.sort(this.sortByVotes('votes'));
+      this.setState({ comments: sortedArray });
+    } else if (e == "3") {
+      const sortedArray = allComments.sort(this.sortByTwoFields('votes', 'timestamp'));
+      this.setState({ comments: sortedArray });
     }
-
   }
 
   /**
    * Sorts the feedback by the time they are committed
    */
-  sortByTime(field){
-      return function(a,b) {
-        return b[field] - a[field]
-      }
+  sortByTime(field) {
+    return function (a, b) {
+      return b[field] - a[field];
+    };
   }
-
 
   /**
    * Sort the feedback by calculating it vote count.
    */
-  sortByVotes(field){
-    return function(a,b){
+  sortByVotes(field) {
+    return function (a, b) {
       return b[field] - a[field];
-    }
+    };
   }
 
   /**
    * Sorts the feedback based on two attribute, including the time and voting count.
    */
-  sortByTwoFields(field1,field2){
-    return function (a,b){
-      if (a[field1] == b[field1]){
+  sortByTwoFields(field1, field2) {
+    return function (a, b) {
+      if (a[field1] == b[field1]) {
         return b[field2] - a[field2];
       }
-      else{
-        return b[field1] - a[field1];
-      }
-    }
+
+      return b[field1] - a[field1];
+    };
   }
 
   render() {
@@ -179,33 +179,36 @@ class CommentBox extends React.Component {
         <div className="commentBox panel panel-default">
           <div className="panel-body">
             <h3>
-              Feedback Board for the Project ID:{" "}
+              Feedback Board for the Project ID:
+              {" "}
               <Badge variant="primary" id="projectID">
                 {this.props.projectID}
               </Badge>
 
             </h3>
 
-            <CommentForm 
+            <CommentForm
               onCommentSubmit={this.handleCommentSubmit}
               projectID={this.props.projectID}
               userID={this.state.userID}
               userName={this.state.userName}
             />
 
-            <Dropdown style={{top:"-40px",float: "right", height: "20px",position:"relative"}}>
+            <Dropdown style={{
+              top: "-40px", float: "right", height: "20px", position: "relative",
+            }}
+            >
               <Dropdown.Toggle variant="success" id="dropdown-basic">
                 Sort the Comments
               </Dropdown.Toggle>
 
               <Dropdown.Menu>
-                <Dropdown.Item onClick={this.sortComments.bind(this,"1")}>Sort by Time</Dropdown.Item>
-                <Dropdown.Item onClick={this.sortComments.bind(this,"2")}>Sort by Votes Count</Dropdown.Item>
-                <Dropdown.Item onClick={this.sortComments.bind(this,"3")}>Defualt</Dropdown.Item>
+                <Dropdown.Item onClick={this.sortComments.bind(this, "1")}>Sort by Time</Dropdown.Item>
+                <Dropdown.Item onClick={this.sortComments.bind(this, "2")}>Sort by Votes Count</Dropdown.Item>
+                <Dropdown.Item onClick={this.sortComments.bind(this, "3")}>Defualt</Dropdown.Item>
               </Dropdown.Menu>
 
             </Dropdown>
-
 
             <CommentList comments={this.state.comments} votedComments={this.state.votedComments} />
           </div>
