dd102621c2c4b697bdfb79ca78d6476b1313ab9e
Karish Singh <karish.singh28@gmail.com>
2021-02-23 14:05:06 +0000

    Rewrote text for the QR page.

--- src/components/feedback-tabs/QRTab.jsx
+++ src/components/feedback-tabs/QRTab.jsx
@@ -19,19 +19,20 @@ class QRTab extends Component {
         </div>
         {/*this is the instruction for the PC device*/}
         <div id="instructionDiv" className="d-sm-block d-none">
-          <h3 className="QRTab_Title">QRcode Using Instructions</h3>
+          <h3 className="QRTab_Title">QR Code  Instructions</h3>
           {/* <p>In order to successfully run the prototype, please do the following steps</p> */}
           <ol>
-            <p>In order to successfully run the prototype, please do the following steps</p>
+            <p>To run this prototype on your device, do the following steps:</p>
 
             <li>
-              To run this app, You are required to install the"
+              Install the "
               <a href="https://expo.io/tools">Expo Go</a>
-              " App on your personal devices
+              " app on your mobile device.
             </li>
-            <li>After opening the App, select the "Scan QR Code" option on the top of the sceen</li>
-            <li>Using the device cemera to scan the QRcode on the imagineThis website</li>
-            <li>The builed prototype will be running on your phone soon</li>
+            {/*<li>After opening the App, select the "Scan QR Code" option on the top of the sceen</li>*/}
+            <li>Open your device's built-in camera app and point it at the QR code on this page</li>
+            <li>A notification will appear saying to open the build in Expo. Click on this.</li>
+            <li>The expo app should then open and the prototype should begin to run on your device.</li>
             <p>
               <br />
               Please check for other related details：
@@ -42,18 +43,18 @@ class QRTab extends Component {
         </div>
         {/* this is the instruction for the mobile device */}
         <div id="instructionDiv" className="d-xl-none">
-          <h3 className="QRTab_Title">QRcode Using Instructions</h3>
+          <h3 className="QRTab_Title">QR Code Instructions</h3>
           {/* <p>In order to successfully run the prototype, please do the following steps</p> */}
           <ol>
-            <p>In order to successfully run the prototype, please do the following steps</p>
+            <p>In run the prototype your device, please do the following steps:</p>
 
             <li>
-              To run this app, You are required to install the"
+              Install the "
               <a href="https://expo.io/tools">Expo Go</a>
-              " App on your personal devices
+              " on app on your mobile device
             </li>
-            <li>Click on the QR code which can redirect to the Expo Application</li>
-            <li>The builed prototype will be running on your phone soon</li>
+            <li>Then click on the QR Code on this page. This should open the expo app</li>
+            <li>The prototype will then begin running on your device</li>
             <p>
               <br />
               Please check for other related details：
